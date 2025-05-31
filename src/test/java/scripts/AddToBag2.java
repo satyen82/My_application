f77af45c78768b276b49aec76b1891ecdb8e43ca
satyen82 <s.j.inamdar1@gmail.com>
2025-05-30 21:34:39 -0400

    test

---
+++ src/test/java/scripts/AddToBag2.java
@@ -0,0 +1,32 @@
+package scripts;
+
+
+
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import genericlib.BaseTest;
+import pages.CartPage;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.ProductDetailsPage;
+import pages.ProductPage;
+
+
+public class AddToBag2 extends BaseTest{
+	
+	
+@Test
+	public static void addToBag() throws InterruptedException { 	
+	HomePage hp = new HomePage (driver);
+	ProductPage lp = new ProductPage (driver);
+	ProductDetailsPage dt = new ProductDetailsPage (driver);
+	CartPage lc = new CartPage (driver);
+	hp.homie();
+	lp.prod();
+	dt.prodDetails();
+	lc.cart("abc@gmail.com");
+	
+	}
+
+}
